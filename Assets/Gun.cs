4b47ef3060ef2a576f7c1bfffecea6a767c7716b
Moses <paekonde@gmail.com>
2020-06-11 06:19:23 +0200

    Initial backup of the game
    The first backup of the game after a lot of details have been added to it

---
+++ Assets/Gun.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	public float damage =10f;
+	public float range = 100f;
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	public ParticleSystem bulletTrace;
+	public GameObject impactEffect;
+	public float impactForce =10f;
+
+	public float fireRate = 15f;
+	private float nextTimeToFire = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+        	nextTimeToFire = Time.time + 1f/fireRate;
+        	Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+    	if(weaponSwitch.selectedWeapon == 1)
+    	{
+    		FindObjectOfType<AudioManager>().Play("Shotgun Shot");
+    	}
+    	else if(weaponSwitch.selectedWeapon == 0)
+    	{
+    		FindObjectOfType<AudioManager>().Play("Alien Shot");
+    	}
+    	else
+    	{
+    		FindObjectOfType<AudioManager>().Play("Shot");
+    	}
+        
+    	muzzleFlash.Play();
+    	//bulletTrace.Play();
+    	RaycastHit hit;
+    	if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+    	{
+    		enemy target = hit.transform.GetComponent<enemy>();
+    		if(target != null)
+    		{
+    			target.TakeDamage(damage);
+    		}
+
+    		if(hit.rigidbody != null)
+    		{
+    			hit.rigidbody.AddForce(-hit.normal * impactForce);
+    		}
+
+    		GameObject impactGO = Instantiate(impactEffect,hit.point,Quaternion.LookRotation(hit.normal));
+    		Destroy(impactGO, 2f);
+    	}
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text zombieLeft;
+    public static int count = 10;
+
+    void Start()
+    {
+        zombieLeft = GetComponent<Text>();
+        zombieLeft.text = count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zombieLeft.text = count.ToString();
+    }
+}
